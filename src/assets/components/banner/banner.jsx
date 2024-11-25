5c3789008834135e2f1482217532d5f995147bf9
Aditya5ingh <adisingh2348123@gmail.com>
2024-11-25 14:38:10 +0530

    usage of best container

--- src/assets/components/banner/banner.jsx
+++ src/assets/components/banner/banner.jsx
@@ -7,29 +7,32 @@ import flipkart from "../../images/flipkart.png";
 export default function Banner() {
   return (
     <div className="banner">
-      <div className="left-banner">
-        <div className="top-left">
-          <h1>Your Feet deserve the best</h1>
-        </div>
-        <div className="bottom-left">
-          <p>
-            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.
-          </p>
-          <button className="but1">Shop Now</button>
-          <button className="but2">Category</button>
-          <div className="gotoLink">
-            <h6>Also available on</h6>
-            <div className="mentionLink">
-              <img src={flipkart} alt="" />
-              <img src={amazon} alt="" />
+      <div className="container">
+        <div className="left-banner">
+          <div className="bottom-left">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>
+              YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+              SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH
+              OUR SHOES.
+            </p>
+            <div className="shop_now">
+              <button className="but1">Shop Now</button>
+              <button className="but2">Category</button>
+            </div>
+
+            <div className="gotoLink">
+              <h6>Also available on</h6>
+              <div className="mentionLink">
+                <img src={flipkart} alt="" />
+                <img src={amazon} alt="" />
+              </div>
             </div>
           </div>
+          <div className="right-banner">
+            <img src={shoe_image} alt="shoe image" />
+          </div>
         </div>
-      </div>
-      <div className="right-banner">
-        <img src={shoe_image} alt="shoe image" />
       </div>
     </div>
   );
--- src/assets/components/navbar/navbar.jsx
+++ src/assets/components/navbar/navbar.jsx
@@ -5,27 +5,31 @@ import "./navbar.css";
 function NavBar() {
   return (
     <div className="navBar">
-      <div className="logo">
-        <img src={p1} alt="brandlogo" />
-      </div>
-      <div className="navMenu">
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">LOcation</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <div className="loginbut">
-        <button href="">Login</button>
+      <div className="container">
+        <div className="menu_wrapper">
+          <a className="logo" href="#">
+            <img src={p1} alt="brandlogo" />
+          </a>
+          <div className="navMenu">
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">LOcation</a>
+              </li>
+              <li>
+                <a href="">About</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="loginbut">
+            <button>Login</button>
+          </div>
+        </div>
       </div>
     </div>
   );
